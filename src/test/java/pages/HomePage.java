9fd07aae702eb18803bbeec128250052fff84669
vladyslavsydun <vladyslavsydun@gmail.com>
2023-07-04 17:57:30 +0300

    Update test

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,6 +3,8 @@ package pages;
 import elements.HomeElements;
 import org.openqa.selenium.WebDriver;
 
+import static base.CommonActions.moveCursor;
+
 public class HomePage extends HomeElements {
 
     public HomePage(WebDriver driver) {
@@ -15,6 +17,15 @@ public class HomePage extends HomeElements {
     }
     public HomePage closeCookie() {
         waitUntilElementToBeClickableByXpath(CLOSE_COOKIE).click();
+        return this;
+    }
+    public HomePage moveCursorToDepartmentBtn() {
+        moveCursor(getDepartmentBtn(), driver);
+        return this;
+    }
+
+    public HomePage clickPoshukViddilenniaZaNomerom() {
+        getPoshukViddilenniaZaNomerom().click();
         return this;
     }
 }
