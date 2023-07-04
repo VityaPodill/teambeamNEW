53ee0d4231a6e6785ca884982eb6f362eaa08462
vladyslavsydun <vladyslavsydun@gmail.com>
2023-07-04 17:38:26 +0300

    Created autotest. Test Case 1, for "department" category"

--- src/test/java/elements/HomeElements.java
+++ src/test/java/elements/HomeElements.java
@@ -11,9 +11,17 @@ public class HomeElements extends AbstractBasePage {
     }
 
     private static final String NOVA_POST_BTN = "//a[text()='Nova Post']";
+    private static final String DEPARTMENT_BTN = "//a[contains(text (), 'Відділення')]";
+    protected final static String CLOSE_COOKIE = "//div[@id='popup_info']//i[@class='click close btn_x']";
+    protected final static String POSHUK_VIDDILENNIA_ZA_NOMEROM = "//a[contains(text (), 'Пошук відділення за номером')]";
+
     protected WebElement getNovaPostElement() {
         return waitUntilElementToBeClickableByXpath(NOVA_POST_BTN);
     }
-    protected final static String CLOSE_COOKIE = "//div[@id='popup_info']//i[@class='click close btn_x']";
-
+    protected WebElement getPoshukViddilenniaZaNomerom() {
+        return waitUntilElementToBeClickableByXpath(POSHUK_VIDDILENNIA_ZA_NOMEROM);
+    }
+    protected WebElement getDepartmentBtn() {
+        return waitUntilElementToBeVisibleByXpath(DEPARTMENT_BTN);
+    }
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,6 +3,8 @@ package pages;
 import elements.HomeElements;
 import org.openqa.selenium.WebDriver;
 
+import static base.CommonActions.moveCursor;
+
 public class HomePage extends HomeElements {
 
     public HomePage(WebDriver driver) {
@@ -13,8 +15,19 @@ public class HomePage extends HomeElements {
         getNovaPostElement().click();
         return this;
     }
+
     public HomePage closeCookie() {
         waitUntilElementToBeClickableByXpath(CLOSE_COOKIE).click();
+        return this;
+    }
+
+    public HomePage moveCursorToDepartmentBtn() {
+        moveCursor(getDepartmentBtn(), driver);
+        return this;
+    }
+
+    public HomePage clickPoshukViddilenniaZaNomerom() {
+        getPoshukViddilenniaZaNomerom().click();
         return this;
     }
 }
